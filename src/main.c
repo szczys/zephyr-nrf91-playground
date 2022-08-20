9a58a4e54c6c7a59e7228647dad28ca211a5dacf
Mike Szczys <szczys@hotmail.com>
2022-08-20 13:33:02 -0500

    Working example reads MCP3201 over SPI

--- src/main.c
+++ src/main.c
@@ -6,56 +6,34 @@
 
 #include <zephyr/zephyr.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(basic_spi, LOG_LEVEL_DBG);
+
+#define SPI_OP	SPI_OP_MODE_MASTER | SPI_MODE_CPOL | SPI_MODE_CPHA | SPI_WORD_SET(8) | SPI_LINES_SINGLE
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
-#define CS_NODE DT_ALIAS(cs0)
 
 /* https://stackoverflow.com/a/69152884 */
 #include <drivers/gpio.h>
 #include <drivers/spi.h>
 
-const struct device *spi_dev;
-
 /* likely we could use this example:
  * https://docs.zephyrproject.org/latest/hardware/peripherals/spi.html#c.SPI_CS_CONTROL_PTR_DT
  * Instead of the following code: */
-
-struct spi_cs_control spi_cs = {
-    /* PA4 as CS pin */
-    .gpio_dev = DEVICE_DT_GET(DT_NODELABEL(gpio0)),
-    .gpio_pin = 1,
-    .gpio_dt_flags = GPIO_ACTIVE_LOW,
-    /* delay in microseconds to wait before starting the transmission and before releasing the CS line */
-    .delay = 10,
-};
-
-#define SPI_CS (&spi_cs)
-
-struct spi_config spi_cfg = {
-    .frequency = 350000,
-    .operation = SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB | SPI_WORD_SET(8) | SPI_LINES_SINGLE | SPI_LOCK_ON,
-    .cs = SPI_CS,
-};
-
-void spi_init()
-{
-    spi_dev = device_get_binding("SPI_2");
-}
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-static const struct gpio_dt_spec cs = GPIO_DT_SPEC_GET(CS_NODE, gpios);
 
 void main(void)
 {
-	printk("Started SPI demo\n");
+	LOG_INF("Started SPI demo");
 	int ret;
 
 	if (!device_is_ready(led.port)) {
@@ -66,39 +44,30 @@ void main(void)
 	if (ret < 0) {
 		return;
 	}
+	const struct spi_dt_spec mcp3201_dev =
+		SPI_DT_SPEC_GET(DT_NODELABEL(mcp3201), SPI_OP, 0);
 
-	gpio_pin_configure_dt(&cs, GPIO_OUTPUT_ACTIVE);
-
-	uint8_t my_buffer[10] = {0};
-	struct spi_buf my_spi_buffer = { my_buffer, 10 };
-	struct spi_buf_set rx_buff = { &my_spi_buffer, 10 };
-	spi_init();
-	gpio_pin_set_dt(&cs, 0);
-
-// 	const struct spi_dt_spec *spi_dev = SPI_DT_SPEC_GET(DT_NODELABEL(spidev),
-// 			SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB | SPI_WORD_SET(8) |
-// 			SPI_LINES_SINGLE | SPI_LOCK_ON,
-// 			10);
-
+	LOG_INF("spi_cs.bus = %p", mcp3201_dev.bus);
+	LOG_INF("spi_cs.config.cs->gpio.port = %p", mcp3201_dev.config.cs->gpio.port);
+	LOG_INF("spi_cs.config.cs->gpio.pin = %u", mcp3201_dev.config.cs->gpio.pin);
 	
+	uint8_t my_buffer[10] = {0};
+	struct spi_buf my_spi_buffer[1];
+	my_spi_buffer[0].buf = my_buffer;
+	my_spi_buffer[0].len = 10;
+	const struct spi_buf_set rx_buff = { my_spi_buffer, 1 };
+
 	while (1) {
-// 		gpio_pin_set_dt(&led, 0);
-		
-		gpio_pin_set_dt(&cs, 1);
-		k_sleep(K_MSEC(100));
-		spi_read(spi_dev, &spi_cfg, &rx_buff);
-		k_sleep(K_MSEC(100));
-		gpio_pin_set_dt(&cs, 0);
-		printk("Received 10 bytes:");
-		printk("%d %d %d %d %d %d %d %d %d %d\n",
+		ret = spi_read_dt(&mcp3201_dev, &rx_buff);
+		LOG_INF("spi_read status: %d", ret);
+		LOG_INF("Received 10 bytes:");
+		LOG_INF("%d %d %d %d %d %d %d %d %d %d",
 					my_buffer[0],my_buffer[1],my_buffer[2],
 					my_buffer[3],my_buffer[4],my_buffer[5],
 					my_buffer[6],my_buffer[7],my_buffer[8],
 					my_buffer[9]
 				);
-// 		gpio_pin_set_dt(&led, 1);
 
-	// 101 111010111 101
 		ret = gpio_pin_toggle_dt(&led);
 		if (ret < 0) {
 			return;
